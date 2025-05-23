0a4729362b66d82cd24b68291c6092ecfddc05df
Abel Semahegn <abel249733@gmail.com>
2025-05-23 10:23:06 -0500

    init will move to dev branch and publish when complete site is up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/theme-provider"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,11 +24,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-serif`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="w-full h-fit sticky top-0 z-10">
+            <Navbar/>
+          </div>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image"
+import Logo from "~/Logo.svg"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { CiShoppingCart } from "react-icons/ci";
+import Link from "next/link";
+export default function Navbar() {
+    return (
+        <div className="w-full h-fit flex justify-between bg-background shadow-xl p-2">
+<Link href={"/"}>
+
+
+            <div className="h-full flex items-center justify-center"><Image src={Logo} width={100} height={50} alt="icon" /></div>
+</Link>
+
+
+            <Sheet>
+                <SheetTrigger>
+                    <CiShoppingCart className="size-12  text-accent-foreground " />
+                </SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle>Are you absolutely sure?</SheetTitle>
+                        <SheetDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+
+        </div>
+    )
+}
